d5a10b4682d02ed77e32be2b4d9ca1cc50ef476d
Clint Egargo <67180945+egargo@users.noreply.github.com>
2025-07-27 16:40:31 +0800

    Initial commit

---
+++ system/common/default.nix
@@ -0,0 +1,12 @@
+{
+  imports = [
+    ./environment.nix
+    ./fonts.nix
+    ./internationalization.nix
+    ./networking.nix
+    ./packages.nix
+    ./shell.nix
+    ./starship.nix
+    ./users.nix
+  ];
+}
