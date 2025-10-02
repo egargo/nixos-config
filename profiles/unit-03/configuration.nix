c9971b2e62c001dec606535eaccb8855edf887d9
Clint Egargo <67180945+egargo@users.noreply.github.com>
2025-10-03 01:01:59 +0800

    Change kernel from latest to lts

--- profiles/unit-03/configuration.nix
+++ profiles/unit-03/configuration.nix
@@ -17,7 +17,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
--- profiles/unit-04/configuration.nix
+++ profiles/unit-04/configuration.nix
@@ -8,7 +8,7 @@
   ];
 
   boot = {
-    kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages;
     loader = {
       systemd-boot.enable = true;
       efi.canTouchEfiVariables = true;
