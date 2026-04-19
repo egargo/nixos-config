20470f1bb0888d8fe07d7af10a6d8e183484f073
Clint Egargo <67180945+egargo@users.noreply.github.com>
2026-04-20 00:32:08 +0800

    Add vscode extensions

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -4,7 +4,11 @@
   programs.vscode = {
     enable = true;
     profiles.default.extensions = with pkgs.vscode-extensions; [
+      eamodio.gitlens
       ms-python.python
+      ms-python.isort
+      ms-python.black-formatter
+      vscodevim.vim
     ];
   };
 }
