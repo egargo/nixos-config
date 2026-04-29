ee61729da0e7c20475596eeb71a45e9bd53b9927
Clint Egargo <67180945+egargo@users.noreply.github.com>
2026-04-29 23:29:41 +0800

    Add ext4 disko

---
+++ disko/ext4.nix
@@ -0,0 +1,39 @@
+{
+  disko.devices = {
+    disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            size "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
+            };
+          };
+          plainSwap = {
+            size = "32G";
+            content = {
+              type = "swap";
+              discardPolicy = "both";
+              resumeDevice = true;
+            };
+          };
+        };
+      };
+    };
+  };
+}
